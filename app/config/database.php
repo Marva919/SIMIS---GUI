723b6171549be5283f7abbe5b5852b763b10ab92
marcbrauchthilfe <marc.filterbox@gmail.com>
2026-04-29 10:40:48 +0200

    Topic: edit database config file
    - edit file for database connectiono

--- app/config/database.php
+++ app/config/database.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "simis");
+$conn = new mysqli("rs03-db-inf-min.ad.fh-bielefeld.de", "MbelhausSIMIS@ORCL", "simis_Sql67", "simis");
 
 if ($conn->connect_error) {
     die("Fehler: " . $conn->connect_error);
